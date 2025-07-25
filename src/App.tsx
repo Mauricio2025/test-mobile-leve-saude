53dd959d25109185ac65b32e3716dd5ba78a2b4b
Mauricio2025 <mauriciiosouza51@gmail.com>
2025-07-24 22:02:53 -0300

    Adiciona React Navigation básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <AppNavigator/>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
---
+++ src/navigation/stack.tsx
@@ -0,0 +1,34 @@
+// src/navigation/stack.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import FeedbackForm from '../screens/FeedbackForm';
+import FeedbackList from '../screens/FeedbackList';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Form: undefined;
+  List: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Form" component={FeedbackForm} />
+        <Stack.Screen name="List" component={FeedbackList} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
